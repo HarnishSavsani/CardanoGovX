1734ad86f86fbe8b9a8633a7bc3c0bd612f256db
Harnish Savsani <68416821+HarnishSavsani@users.noreply.github.com>
2023-08-26 10:16:50 +0530

    Blockchain Contract Oprations

---
+++ blockchain/constants.ts
@@ -0,0 +1,31 @@
+export const VotingHubAddress = {
+    Networks: {
+        51: {
+            'CardanoGovX' : '0x620dA85f3C8A8B9f1046693180017755040722cE',
+            'Cardano' : '0x6aDa1D2a9E55BEbD4A4f3Dbe82A760803418cB63',
+        }
+    }
+};
+
+export const DeploymentBlock = {
+    Networks: {
+        51: {
+            'CardanoGovX' : 53395241,
+            'Cardano' : 42807132,
+        }
+    }
+}
+
+export const RpcEndpoint = {
+    Networks: {
+        50: "https://erpc.xinfin.network",
+        51: "https://erpc.apothem.network",
+    }
+};
+
+export const ExplorerUrl = {
+    Networks: {
+        50: "https://explorer.xinfin.network/address/",
+        51: "https://explorer.apothem.network/address/",
+    }
+}
